2c80b5276d683cbfc7cfa9d4190b4877b087f539
Manish Kumar <mailtomanish.kr2011@gmail.com>
2024-09-02 21:07:55 +0530

    uncia-division-accumulate committed

---
+++ src/main/java/com/uncia/unciadivisionaccumulate/UnciaDivisionAccumulateApplication.java
@@ -0,0 +1,14 @@
+package com.uncia.unciadivisionaccumulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnciaDivisionAccumulateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnciaDivisionAccumulateApplication.class, args);
+		System.out.println("Testing!!");
+	}
+
+}
---
+++ src/test/java/com/uncia/unciadivisionaccumulate/UnciaDivisionAccumulateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncia.unciadivisionaccumulate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnciaDivisionAccumulateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
